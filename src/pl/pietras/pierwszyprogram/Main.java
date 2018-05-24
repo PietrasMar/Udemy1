7692997fcfb3e9a10575d9f1b8773bb358b5bf32
Pietras <pietra39@gmail.com>
2018-05-24 19:00:29 +0200

    first commit hehe

--- src/pl/pietras/pierwszyprogram/Main.java
+++ src/pl/pietras/pierwszyprogram/Main.java
@@ -1,8 +1,11 @@
 package pl.pietras.pierwszyprogram;
 
-
+//comment
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World, Intelij");
+    /*
+    * hehehe
+    * */
     }
 }
